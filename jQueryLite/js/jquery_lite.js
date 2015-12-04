3c25b46849378923bde67e44fe4dd6070dd2760a
Mack Siu <msiu23@gmail.com>
2015-12-03 16:11:43 -0800

    started ajax

--- jQueryLite/js/jquery_lite.js
+++ jQueryLite/js/jquery_lite.js
@@ -1,37 +1,70 @@
+/*global $l */
+/* eslint: no-unused-vars: 1 */
+
+
 (function() {
   'use strict';
-  var DOMNodeCollection = function(array){
+  function DOMNodeCollection(array){
     this.HTMLels = array;
+  }
+
+  var que = [];
+  window.$l = function (arg){
+    if (arg instanceof Function) {
+      que.push(arg);
+    }
+
+    if (typeof arg === 'string'){
+      var nodeList = document.querySelectorAll(arg);
+      var nodeListArray = [].slice.call(nodeList);
+      return new DOMNodeCollection(nodeListArray);
+    } else if (arg instanceof HTMLElement ){
+      var HTMLArray = [].slice.call(arg);
+      return new DOMNodeCollection(HTMLArray);
+    }
   };
 
-  if (typeof $l === "undefined") {
-    var que = [];
-    window.$l = function (arg){
-      if (arg instanceof Function) {
-        que.push(arg);
-      }
-
-      if (typeof arg === 'string'){
-        var nodeList = document.querySelectorAll(arg);
-        var nodeListArray = [].slice.call(nodeList);
-        return new DOMNodeCollection(nodeListArray);
-      } else if (arg instanceof HTMLElement ){
-        var HTMLArray = [].slice.call(arg);
-        return new DOMNodeCollection(HTMLArray);
-      }
-    };
-    if (document.readyState === "complete") {
+  if (document.readyState === "complete") {
+    // this code isn't doing anything
+    que.forEach(function(fn) {
+      fn();
+    });
+  } else {
+    document.addEventListener("DOMContentLoaded", function() {
       que.forEach(function(fn) {
-        fn();
-      });
-    } else {
-      document.addEventListener("DOMContentLoaded", function() {
-        que.forEach(function(fn) {
         fn();
       });
     });
   }
-}
+
+  // this callback will never be called if document is already ready
+  $l(function () {});
+
+  var extend = $l.extend = function (base) {
+    var extensions = Array.prototype.slice.call(arguments, 1);
+
+    extensions.forEach(function(obj) {
+        for (var key in obj) {
+          if (obj.hasOwnProperty(key)) {
+            base[key] = obj[key];
+          }
+        }
+      });
+  };
+
+  var ajax = $l.ajax = function(options){
+    var deFault = {
+      method: 'Get',
+      success: function(){console.log("success");},
+      error: function(){console.log("error");},
+      data: null,
+      contentType: 'application/x-www-form-urlencoded; charset=UTF-8',
+      url: ""
+    };
+
+    $l.extend(deFault, options);
+    // success, error, url, method, data, and contentType
+  };
     /* DRYs out the code
       arg = document.querySelectorAll(arg);
     var newArray = [].slice.call(arg);
@@ -49,9 +82,7 @@
   };
 
   DOMNodeCollection.prototype.empty = function () {
-    this.HTMLels.forEach(function(el) {
-      el.innerHTML = null;
-    });
+    this.html('');
   };
 
   DOMNodeCollection.prototype.append = function (arg) {
@@ -60,6 +91,7 @@
         el.innerHTML += arg;
       });
     } else if(arg instanceof HTMLElement){
+      // append to first only
       this.HTMLels.forEach(function(el) {
         el.appendChild(arg);
       });
@@ -73,7 +105,7 @@
   };
 
   DOMNodeCollection.prototype.attr = function (string) {
-
+      // https://developer.mozilla.org/en-US/docs/Web/API/NamedNodeMap
     for (var i = 0; i < this.HTMLels.length; i++) {
       for (var j = 0; i < this.HTMLels[i].attributes.length; j++) {
         if (this.HTMLels[i].attributes[j].name === string){
@@ -129,43 +161,43 @@
     return new DOMNodeCollection(myThreeDads);
   };
 
-DOMNodeCollection.prototype.find = function (arg) {
-  var matches = [];
-  var nodeList = document.querySelectorAll(arg);
-  var nodeListArray = [].slice.call(nodeList);
-  this.HTMLels.forEach( function(el){
-    nodeListArray.forEach(function(node){
-      if (el.contains(node)){
-        matches.push(node);
-      }
+  DOMNodeCollection.prototype.find = function (arg) {
+    var matches = [];
+    var nodeList = document.querySelectorAll(arg);
+    var nodeListArray = [].slice.call(nodeList);
+    this.HTMLels.forEach( function(el){
+      nodeListArray.forEach(function(node){
+        if (el.contains(node)){
+          matches.push(node);
+        }
+      });
     });
-  });
-  return new DOMNodeCollection(matches);
-};
+    return new DOMNodeCollection(matches);
+  };
 
-DOMNodeCollection.prototype.remove = function (arg) {
-  var matched = null;
+  DOMNodeCollection.prototype.remove = function (arg) {
+    var matched = null;
 
-  if (typeof arg !== "undefined") {
-    matched = this.find(arg).HTMLels;
-  } else {
-    matched = this.HTMLels;
-  }
+    if (typeof arg !== "undefined") {
+      matched = this.find(arg).HTMLels;
+    } else {
+      matched = this.HTMLels;
+    }
 
-  matched.forEach(function(el) {
-    el.parentNode.removeChild(el);
-  });
-};
+    matched.forEach(function(el) {
+      el.parentNode.removeChild(el);
+    });
+  };
 
-DOMNodeCollection.prototype.on = function (type, fn) {
-  this.HTMLels.forEach( function(el){
-    el.addEventListener( type, fn);
-  });
-};
+  DOMNodeCollection.prototype.on = function (type, fn) {
+    this.HTMLels.forEach( function(el){
+      el.addEventListener( type, fn);
+    });
+  };
 
-DOMNodeCollection.prototype.off = function (type, fn) {
-  this.HTMLels.forEach( function(el){
-    el.removeEventListener( type, fn);
-  });
-};
+  DOMNodeCollection.prototype.off = function (type, fn) {
+    this.HTMLels.forEach( function(el){
+      el.removeEventListener( type, fn);
+    });
+  };
 }());
